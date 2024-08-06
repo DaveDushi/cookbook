a74f74608069f2ad837a301c83d21bb2e45cf069
DaveDushi <david.dusi@hotmail.com>
2024-08-06 14:53:57 +0300

    updated ui

--- src/app/recipeform/page.tsx
+++ src/app/recipeform/page.tsx
@@ -1,65 +1,101 @@
 'use client'
 import React, { useState } from 'react';
 
+const cuisineList = [ 
+    'Israeli', 'Mexican', 'Chinese', 'Texas Ranch', 'Italian', 'Indian', 'Thai', 
+    'Japanese', 'French', 'Greek', 'Mediterranean', 'Spanish', 'Korean', 
+    'Vietnamese', 'Middle Eastern', 'Caribbean', 'American', 'African', 'German', 
+    'Russian', 'Brazilian', 'Turkish'
+  ];
+  
+const mealList = ['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Dessert'];
+
+const skillList = ['Beginner', 'Intermediate', 'Expert'];
+
 const RecipeForm: React.FC = () => {
-  const [otherIngredient, setOtherIngredient] = useState('');
-  const [isOtherChecked, setIsOtherChecked] = useState(false);
   const [cookingTime, setCookingTime] = useState(30);
-  const [servings, setServings] = useState(1);
+  const [servings, setServings] = useState(2);
+  const [email, setEmail] = useState('');
+  const [ingredients, setIngredients] = useState<string[]>(['']);
+  const [marketingEmail, setMarketingEmail] = useState<boolean>(true);
 
-  const cuisineList = [ 'Israeli',
-    'Mexican', 'Chinese', 'Texas Ranch', 'Italian', 'Indian', 'Thai', 'Japanese', 
-    'French', 'Greek', 'Mediterranean', 'Spanish', 'Korean', 'Vietnamese', 
-    'Middle Eastern', 'Caribbean', 'American', 'African', 'German', 'Russian', 
-    'Brazilian', 'Peruvian', 'Turkish'
-  ];
+  const handleIngredientChange = (index: number, value: string) => {
+    const newIngredients = [...ingredients];
+    newIngredients[index] = value;
+    setIngredients(newIngredients);
+  };
 
-  const fridgeList = [
-    'Milk', 'Eggs', 'Butter', 'Cheese', 'Tomatoes', 'Chicken', 'Rice', 'Pasta',
-    'Bread', 'Onions', 'Garlic', 'Bell Peppers', 'Beef', 'Fish', 'Carrots',
-    'Salt', 'Pepper', 'Paprika', 'Cumin', 'Turmeric', 'Oregano', 'Basil', 'Thyme',
-    'Rosemary', 'Cinnamon', 'Nutmeg', 'Ginger', 'Chili Powder', 'Coriander',
-    'Ketchup', 'Mustard', 'Mayonnaise', 'Soy Sauce', 'Hot Sauce', 'Vinegar',
-    'Barbecue Sauce', 'Teriyaki Sauce', 'Olive Oil', 'Sesame Oil', 'Honey'
-  ];
+  const addIngredient = () => {
+    setIngredients([...ingredients, '']);
+  };
+
+  const removeIngredient = (index: number) => {
+    const newIngredients = [...ingredients];
+    newIngredients.splice(index, 1);
+    setIngredients(newIngredients);
+  };
+
+
+  const handleSubmit = async (event?: React.FormEvent<HTMLFormElement>) => {
+    if (event) event.preventDefault();
+    alert('Form submitted')
+  };
   
-  
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLFormElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevent form submission on Enter key press
+    }
+  };
+
 
   return (
     <div className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-8 mt-12">
       <h2 className="text-3xl font-bold text-gray-800 mb-6">Find Your Perfect Recipe</h2>
       
-      <form>
+      <form onSubmit={handleSubmit} onKeyDown={handleKeyDown}>
+        {/* Meal type */}
+        <div className="mb-6">
+          <label className="block text-lg font-medium text-gray-700 mb-2">
+            What are you making?
+          </label>
+          <select className="form-select text-md mt-1 block border border-gray-300 rounded-md">
+            {mealList.map(meal => (
+              <option key={meal} value={meal}>{meal}</option>
+            ))}
+          </select>
+        </div>
+        
         {/* What's in your fridge/closet */}
         <div className="mb-6">
           <label className="block text-lg font-medium text-gray-700 mb-2">
-            What's in your fridge/closet?
+            Which ingredients/spices would you like to use?
           </label>
-          <div className="flex flex-wrap gap-3">
-            {fridgeList.map(item => (
-              <label key={item} className="inline-flex items-center">
-                <input type="checkbox" className="form-checkbox text-green-600" />
-                <span className="ml-2 text-gray-700">{item}</span>
-              </label>
-            ))}
-            <label className="inline-flex items-center">
-              <input 
-                type="checkbox" 
-                className="form-checkbox text-green-600" 
-                onChange={(e) => setIsOtherChecked(e.target.checked)} 
-              />
-              <span className="ml-2 text-gray-700">Other</span>
-            </label>
-            {isOtherChecked && (
+          {ingredients.map((ingredient, index) => (
+            <div key={index} className="flex items-center mb-2">
               <input 
                 type="text" 
-                className="form-input mt-2 block w-full border border-gray-300 rounded-md" 
-                placeholder="Enter other ingredients"
-                value={otherIngredient}
-                onChange={(e) => setOtherIngredient(e.target.value)} 
+                className="form-input mt-1 block w-1/4 border border-gray-300 rounded-md" 
+                placeholder="Enter an ingredient"
+                value={ingredient}
+                onChange={(e) => handleIngredientChange(index, e.target.value)}
               />
-            )}
-          </div>
+              <button 
+                type="button" 
+                onClick={() => removeIngredient(index)} 
+                className="ml-2 text-lg text-red-600 hover:text-red-800"
+              >
+                ×
+              </button>
+            </div>
+          ))}
+          <button 
+            type="button" 
+            onClick={addIngredient} 
+            className="bg-green-500 text-white text-sm px-3 py-2 rounded-full hover:bg-green-600 transition duration-300"
+          >
+            Add Ingredient
+          </button>
         </div>
 
         {/* Type of cuisine */}
@@ -67,7 +103,7 @@ const RecipeForm: React.FC = () => {
           <label className="block text-lg font-medium text-gray-700 mb-2">
             Which type of cuisine?
           </label>
-          <select className="form-select mt-1 block w-full border border-gray-300 rounded-md">
+          <select className="form-select text-md mt-1 block border border-gray-300 rounded-md">
             {cuisineList.map(cuisine => (
               <option key={cuisine} value={cuisine}>{cuisine}</option>
             ))}
@@ -106,7 +142,7 @@ const RecipeForm: React.FC = () => {
             min="1" 
             value={servings}
             onChange={(e) => setServings(Number(e.target.value))}
-            className="form-input mt-1 block w-full border border-gray-300 rounded-md" 
+            className="form-input mt-1 block border border-gray-300 rounded-md" 
           />
         </div>
 
@@ -115,11 +151,36 @@ const RecipeForm: React.FC = () => {
           <label className="block text-lg font-medium text-gray-700 mb-2">
             Experience level
           </label>
-          <select className="form-select mt-1 block w-full border border-gray-300 rounded-md">
-            {['Beginner', 'Intermediate', 'Expert'].map(level => (
+          <select className="form-select mt-1 block border border-gray-300 rounded-md">
+            {skillList.map(level => (
               <option key={level} value={level}>{level}</option>
             ))}
           </select>
+        </div>
+
+        {/* Email */}
+        <div className="mb-6">
+          <label className="block text-lg font-medium text-gray-700 mb-2">
+            Enter your email adress:
+          </label>
+          <input 
+            type="text" 
+            value={email}
+            required
+            placeholder='example@example.com'
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-input mt-1 block border border-gray-300 rounded-md" 
+          />
+          <label>
+              <input
+                type="checkbox"
+                name="marketingEmail"
+                checked={marketingEmail}
+                onChange={(e) => setMarketingEmail(e.target.checked)}
+                className="mr-2 mt-3"
+              />
+              Would you like to receive marketing emails from us?
+            </label>
         </div>
 
         {/* Submit button */}
